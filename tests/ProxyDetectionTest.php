181e9a6810ea12368b5b27988d4d596b9b512a57
Rob Allen <rob@akrabat.com>
2023-02-17 17:55:39 +0000

    Set minimum PHP version to 8.0

--- tests/ProxyDetectionTest.php
+++ tests/ProxyDetectionTest.php
@@ -3,15 +3,15 @@ namespace RKA\Middleware\Test;
 
 use PHPUnit\Framework\TestCase;
 use RKA\Middleware\ProxyDetection;
-use Zend\Diactoros\Response;
-use Zend\Diactoros\ServerRequestFactory;
+use Laminas\Diactoros\Response;
+use Laminas\Diactoros\ServerRequestFactory;
 
 class ProxyDetectionTest extends TestCase
 {
     public function testSchemeAndHostAndPortWithPortInHostHeader()
     {
         $request = ServerRequestFactory::fromGlobals([
-            'REMOTE_ADDR' => '192.168.0.1',
+            'REMOTE_ADDR' => '1.2.3.4',
             'HTTP_HOST' => 'foo.com',
             'HTTP_X_FORWARDED_PROTO' => 'https',
             'HTTP_X_FORWARDED_HOST' => 'example.com:1234',
@@ -36,7 +36,7 @@ class ProxyDetectionTest extends TestCase
     public function testSchemeAndHostAndPortWithPortInPortHeader()
     {
         $request = ServerRequestFactory::fromGlobals([
-            'REMOTE_ADDR' => '192.168.0.1',
+            'REMOTE_ADDR' => '1.2.3.4',
             'HTTP_HOST' => 'foo.com',
             'HTTP_X_FORWARDED_PROTO' => 'https',
             'HTTP_X_FORWARDED_HOST' => 'example.com',
@@ -62,7 +62,7 @@ class ProxyDetectionTest extends TestCase
     public function testSchemeAndHostAndPortWithPortInHostAndPortHeader()
     {
         $request = ServerRequestFactory::fromGlobals([
-            'REMOTE_ADDR' => '192.168.0.1',
+            'REMOTE_ADDR' => '1.2.3.4',
             'HTTP_HOST' => 'foo.com',
             'HTTP_X_FORWARDED_PROTO' => 'https',
             'HTTP_X_FORWARDED_HOST' => 'example.com:1000',
@@ -88,7 +88,7 @@ class ProxyDetectionTest extends TestCase
     public function testTrustedProxies()
     {
         $request = ServerRequestFactory::fromGlobals([
-            'REMOTE_ADDR' => '192.168.0.1',
+            'REMOTE_ADDR' => '1.2.3.4',
             'HTTP_HOST' => 'foo.com',
             'HTTP_X_FORWARDED_PROTO' => 'https',
             'HTTP_X_FORWARDED_HOST' => 'example.com:1234',
@@ -96,7 +96,7 @@ class ProxyDetectionTest extends TestCase
 
         $response = new Response();
 
-        $middleware = new ProxyDetection(['192.168.0.1']);
+        $middleware = new ProxyDetection(['1.2.3.4']);
         $middleware($request, $response, function ($request, $response) use (&$scheme, &$host, &$port) {
             // simply store the values
             $scheme = $request->getUri()->getScheme();
@@ -110,17 +110,17 @@ class ProxyDetectionTest extends TestCase
         $this->assertSame(1234, $port);
     }
 
-    public function testNonTrustedProxies()
+    public function testNonTrustedProxiesIgnoreXForwardedHost()
     {
         $request = ServerRequestFactory::fromGlobals([
-            'REMOTE_ADDR' => '10.0.0.1',
+            'REMOTE_ADDR' => '4.3.2.1',
             'HTTP_HOST' => 'foo.com',
             'HTTP_X_FORWARDED_HOST' => 'example.com:1234',
         ]);
 
         $response = new Response();
 
-        $middleware = new ProxyDetection(['192.168.0.1']);
+        $middleware = new ProxyDetection(['1.2.3.4']);
         $middleware($request, $response, function ($request, $response) use (&$scheme, &$host, &$port) {
             // simply store the values
             $scheme = $request->getUri()->getScheme();
